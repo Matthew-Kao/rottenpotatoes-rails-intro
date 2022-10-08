159439e9ec76253aeb661bfe910b4d95b8bbe3a8
Matthew Kao <matthewkao@berkeley.edu>
2022-10-08 05:56:16 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,17 @@
 class Movie < ActiveRecord::Base
+
+  has_many :reviews
+
+  def self.with_ratings(ratings_list)
+    if ratings_list.length() != 0
+      tempRating = ratings_list.map{|rating| rating.upcase}
+      Movie.where(rating: tempRating)
+    else
+      Movie.all
+    end
+  end
+
+  def self.all_ratings
+    ['G','PG','PG-13','R']
+  end
 end
